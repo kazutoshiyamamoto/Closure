998f982c72ac7cde43211fb9bab0695bb5368477
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-07 19:36:54 +0900

    クロージャの実行

--- Closure/Closure/ViewController.swift
+++ Closure/Closure/ViewController.swift
@@ -14,24 +14,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var closure: (String) -> Int
-        // 引数と戻り値の型を明示した場合
-        closure = { (string: String) -> Int in
-            return string.count
-        }
-        print(closure("abc"))
-        
-        // 引数と戻り値の型を省略した場合
-        closure = { string in
-            return string.count * 2
-        }
-        print(closure("abc"))
+        print(lengthOfString("I contain 23 characters"))
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    let lengthOfString = { (string: String) -> Int in
+        return string.count
     }
 
 
